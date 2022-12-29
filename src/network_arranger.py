587f6536ac72d603dd3f0fa5703aa6296d5aaf7a
AllaVinner <joel.w.ottosson@gmail.com>
2022-12-29 18:25:13 +0100

    Commented code in network arranger

--- src/network_arranger.py
+++ src/network_arranger.py
@@ -24,16 +24,20 @@ def get_connection_matrix(G, node_to_index):
 
 
 def arrange_graph(G, T = 100, eps = 1e-1, optim = None, verbose = True, general_loss = None, connected_loss = None):
+    """
+    Returns two vectors with the x and y positions of the nodes in G. 
+    G is expected to be a graph from the networkx library.
+    The funstions also returns a stats dictionary with the information about the training process and 
+    a mapping from the node names to the indices in the returned positions. 
+    """
     N = G.number_of_nodes()
     node_to_index, index_to_node = get_index_maps(G)
-    
-    Ig = torch.ones((N,N), dtype=torch.float32)-torch.eye(N, N)
-    Ic = get_connection_matrix(G, node_to_index)
 
-    # Init node coordinates
-    node_x = torch.normal(0, np.sqrt(N), (N,1), requires_grad=True, dtype=torch.float32)
-    node_y = torch.normal(0, np.sqrt(N), (N,1), requires_grad=True, dtype=torch.float32)
-    
+    # Set general and connected mask
+    Mg = torch.ones((N,N), dtype=torch.float32)-torch.eye(N, N)
+    Mc = get_connection_matrix(G, node_to_index)
+
+    # Set optimizer and loss functions    
     if optim is None:
         optim = torch.optim.Adam([node_x, node_y], lr=1.0)
     if general_loss is None:
@@ -41,30 +45,38 @@ def arrange_graph(G, T = 100, eps = 1e-1, optim = None, verbose = True, general_
     if connected_loss is None:
         connected_loss = lf.div_lin
 
+    # Init stats
     stats = {} 
     stats['node_to_index'] = node_to_index
-    stats['connection_matrix'] = Ic
+    stats['connection_matrix'] = Mc
     if verbose:
         stats['loss'] = []
         stats['positions'] = np.zeros((T, N, 2))
         stats['positions'][0, : , 0] = node_x.clone().detach()[:, 0]
         stats['positions'][0, : , 1] = node_y.clone().detach()[:, 0]
 
+    # Init node coordinates
+    node_x = torch.normal(0, np.sqrt(N), (N,1), requires_grad=True, dtype=torch.float32)
+    node_y = torch.normal(0, np.sqrt(N), (N,1), requires_grad=True, dtype=torch.float32)
+
+    # Training
     for t in range(T-1):
+        # Calculate Loss
         R2 = torch.pow(node_x-node_x.T, 2) + torch.pow(node_y-node_y.T, 2)
-        Gen = R2*Ig
-        Con = R2*Ic
+        Gen = R2*Mg
+        Con = R2*Mc
         
-        Eg = general_loss(Gen/N+eps)
-        Ec = connected_loss(Con+eps)
+        Lg = general_loss(Gen/N+eps)
+        Lc = connected_loss(Con+eps)
 
-        E = Eg + Ec
-        L = torch.mean(E)
+        L = torch.mean(Lg + Lc)
 
+        # Update positions
         optim.zero_grad()
         L.backward()
         optim.step()
         
+        # Save stats
         if verbose: 
             stats['loss'].append(L.item())
             stats['positions'][t+1, : , 0] = node_x.clone().detach()[:, 0]
@@ -72,14 +84,13 @@ def arrange_graph(G, T = 100, eps = 1e-1, optim = None, verbose = True, general_
 
     # Get final Loss
     R2 = torch.pow(node_x-node_x.T, 2) + torch.pow(node_y-node_y.T, 2)
-    Gen = R2*Ig
-    Con = R2*Ic
-    
-    Eg = general_loss(Gen/N+eps)
-    Ec = connected_loss(Con+eps)
+    Gen = R2*Mg
+    Con = R2*Mc
+    Lg = general_loss(Gen/N+eps)
+    Lc = connected_loss(Con+eps)
+    L = torch.mean(Lg + Lc)
 
-    E = Eg + Ec
-    L = torch.mean(E)
+    # Save stats
     if verbose: 
         stats['loss'].append(L.item())
 
