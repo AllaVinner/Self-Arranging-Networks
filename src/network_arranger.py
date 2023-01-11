e1e411f968416ea6a444ff8596d8b9ddca097433
AllaVinner <joel.w.ottosson@gmail.com>
2023-01-11 20:19:43 +0100

    Adding stopping criteria for SGD

--- src/network_arranger.py
+++ src/network_arranger.py
@@ -23,7 +23,7 @@ def get_connection_matrix(G, node_to_index):
     return connected
 
 
-def arrange_graph(G, T = 100, eps = 1e-1, verbose = True, general_loss = None, connected_loss = None):
+def arrange_graph(G, T = 100, eps = 1e-1, general_loss = None, connected_loss = None):
     """
     Returns two vectors with the x and y positions of the nodes in G. 
     G is expected to be a graph from the networkx library.
@@ -47,18 +47,20 @@ def arrange_graph(G, T = 100, eps = 1e-1, verbose = True, general_loss = None, c
     node_x = torch.normal(0, np.sqrt(N), (N,1), requires_grad=True, dtype=torch.float32)
     node_y = torch.normal(0, np.sqrt(N), (N,1), requires_grad=True, dtype=torch.float32)
     optim = torch.optim.Adam([node_x, node_y], lr=1.0)
+
     
     # Init stats
     stats = {} 
     stats['node_to_index'] = node_to_index
     stats['connection_matrix'] = Mc
-    if verbose:
-        stats['loss'] = []
-        stats['positions'] = np.zeros((T, N, 2))
-        stats['positions'][0, : , 0] = node_x.clone().detach()[:, 0]
-        stats['positions'][0, : , 1] = node_y.clone().detach()[:, 0]
-
-
+    stats['loss'] = []
+    stats['positions'] = np.zeros((T, N, 2))
+    current_x = node_x.clone().detach()[:, 0]
+    current_y = node_y.clone().detach()[:, 0]
+    stats['positions'][0, : , 0] = current_x
+    stats['positions'][0, : , 1] = current_y
+    stats['movement'] = np.zeros((T,))
+    movement = []
 
     # Training
     for t in range(T-1):
@@ -78,10 +80,15 @@ def arrange_graph(G, T = 100, eps = 1e-1, verbose = True, general_loss = None, c
         optim.step()
         
         # Save stats
-        if verbose: 
-            stats['loss'].append(L.item())
-            stats['positions'][t+1, : , 0] = node_x.clone().detach()[:, 0]
-            stats['positions'][t+1, : , 1] = node_y.clone().detach()[:, 0]
+        prev_x = current_x
+        prev_y = current_y
+        current_x = node_x.clone().detach()[:, 0]
+        current_y = node_y.clone().detach()[:, 0]
+        
+        stats['loss'].append(L.item())
+        stats['positions'][t+1, : , 0] = current_x
+        stats['positions'][t+1, : , 1] = current_y
+        stats['movement'][t+1] =torch.max(torch.sqrt((current_x-prev_x)**2+(current_y-prev_y)**2))
 
     # Get final Loss
     R2 = torch.pow(node_x-node_x.T, 2) + torch.pow(node_y-node_y.T, 2)
@@ -92,10 +99,89 @@ def arrange_graph(G, T = 100, eps = 1e-1, verbose = True, general_loss = None, c
     L = torch.mean(Lg + Lc)
 
     # Save stats
-    if verbose: 
-        stats['loss'].append(L.item())
+    stats['loss'].append(L.item())
 
     return (node_x.clone().detach()[:,0],
             node_y.clone().detach()[:,0],
             stats)
 
+
+def arrange_graph_until_stable(G, step_limit = 0.01, eps = 1e-1, general_loss = None, connected_loss = None):
+    """
+    Returns two vectors with the x and y positions of the nodes in G. 
+    G is expected to be a graph from the networkx library.
+    The funstions also returns a stats dictionary with the information about the training process and 
+    a mapping from the node names to the indices in the returned positions. 
+    """
+    N = G.number_of_nodes()
+    node_to_index, index_to_node = get_index_maps(G)
+
+    # Set general and connected mask
+    Mg = torch.ones((N,N), dtype=torch.float32)-torch.eye(N, N)
+    Mc = get_connection_matrix(G, node_to_index)
+
+    # Set optimizer and loss functions    
+    if general_loss is None:
+        general_loss =  lf.div_log
+    if connected_loss is None:
+        connected_loss = lf.div_lin
+
+    # Init node coordinates
+    node_x = torch.normal(0, np.sqrt(N), (N,1), requires_grad=True, dtype=torch.float32)
+    node_y = torch.normal(0, np.sqrt(N), (N,1), requires_grad=True, dtype=torch.float32)
+    optim = torch.optim.Adam([node_x, node_y], lr=1.0)
+
+    
+    # Init stats
+    stats = {} 
+    stats['node_to_index'] = node_to_index
+    stats['connection_matrix'] = Mc
+    stats['loss'] = []
+    current_x = node_x.clone().detach()[:, 0]
+    current_y = node_y.clone().detach()[:, 0]
+    stats['movement'] = []
+    movement = []
+
+    # Training
+    while True:
+        # Calculate Loss
+        R2 = torch.pow(node_x-node_x.T, 2) + torch.pow(node_y-node_y.T, 2)
+        Gen = R2*Mg
+        Con = R2*Mc
+        
+        Lg = general_loss(Gen/N+eps)
+        Lc = connected_loss(Con+eps)
+
+        L = torch.mean(Lg + Lc)
+
+        # Update positions
+        optim.zero_grad()
+        L.backward()
+        optim.step()
+        
+        # Save stats
+        prev_x = current_x
+        prev_y = current_y
+        current_x = node_x.clone().detach()[:, 0]
+        current_y = node_y.clone().detach()[:, 0]
+        max_step = torch.max(torch.sqrt((current_x-prev_x)**2+(current_y-prev_y)**2)).item()
+
+        stats['loss'].append(L.item())
+        stats['movement'].append(max_step)
+        if max_step < step_limit:
+            break
+
+    # Get final Loss
+    R2 = torch.pow(node_x-node_x.T, 2) + torch.pow(node_y-node_y.T, 2)
+    Gen = R2*Mg
+    Con = R2*Mc
+    Lg = general_loss(Gen/N+eps)
+    Lc = connected_loss(Con+eps)
+    L = torch.mean(Lg + Lc)
+
+    # Save stats
+    stats['loss'].append(L.item())
+
+    return (node_x.clone().detach()[:,0],
+            node_y.clone().detach()[:,0],
+            stats)
